cb994c07b3ca6284df9acccf2ac266c65d01b530
Xinghong Fu <65034819+shinfxh@users.noreply.github.com>
2022-10-23 16:15:21 -0400

    Update Correlation Testing.py

--- Correlation Testing.py
+++ Correlation Testing.py
@@ -6,12 +6,16 @@ import qnt.data as qndata
 import qnt.output as qnout
 import qnt.stats as qns
 
-dataxr = qndata.stocks.load_ndx_data(min_date = "2015-01-01")
 data = dataxr.sel(field = 'close').to_numpy()
 tickers = dataxr['asset'].to_numpy()
 dates = dataxr['time'].to_numpy()
 close = dataxr.sel(field="close").to_numpy()
 close = close.transpose()
-coef = np.corrcoef(close)[0]
-print(coef)
-print(tickers)
+coef = np.corrcoef(close)
+coef = np.nan_to_num(coef)
+n = len(coef)
+coef = coef.flatten()
+most_negative = np.argsort(coef)[: 100]
+most_negative = [(i // n, i % n) for i in most_negative]
+most_negative_tickers = [(tickers[i], tickers[j]) for i, j in most_negative]
+most_negative_tickers
